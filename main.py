d61ff380b3ad03a91104a81064507a3f1b5d65a3
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-03-03 15:51:43 +0000

    boy this one might be tricky

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+#objective, recreate the coffee machine using objects from the provided clasess
+
+#1) create objects of each class and see what they do 
+
+menu =  Menu()
+
+print(menu.get_items())
+
+menu.find_drink()
+
